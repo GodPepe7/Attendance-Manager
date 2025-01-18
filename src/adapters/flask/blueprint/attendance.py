84d9419fa9f0f3d3f39051a0e36975ef0c184469
Alexander Mai <83711446+GodPepe7@users.noreply.github.com>
2025-01-18 21:30:06 +0100

    remove unnecessary code

--- src/adapters/flask/blueprint/attendance.py
+++ src/adapters/flask/blueprint/attendance.py
@@ -58,23 +58,24 @@ def get_qr_code_string(
         lecture_id: int,
         attendance_service: AttendanceService = Provide[Container.attendance_service]
 ):
-    seconds_string = request.args.get('seconds')
-    if not seconds_string:
+    seconds = request.args.get('seconds', type=int)
+    if not seconds:
         return "Missing 'seconds' paremeter in URL", 400
-    try:
-        seconds = int(seconds_string)
-        encypted_expiration_time = attendance_service.generate_qr_code_string(g.user, course_id, lecture_id, seconds,
-                                                                              datetime.now())
-        qr_code_link = url_for(
-            "attendance.save_with_qr_code_string",
-            qr_code_string=encypted_expiration_time
-        )
-        return qr_code_link, 200
-    except ValueError:
-        return "'seconds' input needs to be a valid number", 400
+    encypted_expiration_time = attendance_service.generate_qr_code_string(
+        g.user,
+        course_id,
+        lecture_id,
+        seconds,
+        datetime.now()
+    )
+    qr_code_link = url_for(
+        "attendance.save_with_qr_code_string",
+        qr_code_string=encypted_expiration_time
+    )
+    return qr_code_link, 200
 
 
-@attendance.get("/qr/<qr_code_string>")
+@attendance.get("/attendance/qr/<qr_code_string>")
 @inject
 @login_required()
 def save_with_qr_code_string(
@@ -82,4 +83,20 @@ def save_with_qr_code_string(
         attendance_service: AttendanceService = Provide[Container.attendance_service]
 ):
     attendance_service.save_with_qr_code_string(g.user, qr_code_string, datetime.now())
+    return render_template("success.html")
+
+
+@attendance.post("/attendance")
+@inject
+@login_required()
+def save_with_password(
+        attendance_service: AttendanceService = Provide[Container.attendance_service]
+):
+    password = request.form.get("password")
+    lecture_id = request.form.get("lecture_id", type=int)
+    if not password or lecture_id:
+        return "Need 'password' and 'lecture_id'", 400
+    if lecture_id >= 0:
+        return "'lecture_id' can only be numbers greater than 0", 400
+    attendance_service.save_with_password(g.user, lecture_id, password)
     return render_template("success.html")
